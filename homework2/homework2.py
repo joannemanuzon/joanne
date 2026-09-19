1b7f90465b3d467a34d9c9f84701411403fd5930
Joanne Manuzon <joannemanuzon@berkeley.edu>
2026-09-18 21:30:56 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,101 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+
+# Git is a distributed version control system designed to track changes in source code.
+# It operates locally on your machine, allowing you to manage code history, create branches,
+# and merge changes. Github is a cloud-based platform built on top of Git. It provides a
+# centralized location for hosting Git repositories, enabling collaboration among teams.
+# Gitbash is an application for Microsoft Windwos environments that provides an emulation
+# layer for a Git command-line experience.
+
+# 2) What’s the difference between the terminal and the command line?
+
+# The terminal refers to the GUI window or emulator that hosts the shell session.
+# The command line is where you type commands inside that terminal window.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+
+# The terminal refers to the GUI window or emulator that hosts the shell session. The command line is where you type commands inside that 
+# terminal window.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+
+# Python is the programming language, Conda is a package/environment manager. Anaconda
+# is a distribution that bundles Python + conda + hundreds of scientific packages.
+
+# 5) What is VS Code? 
+
+# VS Code is a free code editor used to write, run, and debug programming code.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+
+# Jupyter Notebook is a simple, document-style interface for writing code + text in small cells.
+# Jupyter Lab is a more advanced, full workspace environment that includes notebook plus file
+# browsers, terminals, text editors, and multiple tabs.
+
+# 7) What does ~/ mean?
+
+# ~/ means your home directory in Git Bash, Linux, and macOS.
+
+# 8) What’s the difference between an absolute path and a relative path?
+
+# The absolute path is the full address of a file or folder, starting from the root of the file
+# system. The relative path is the shortcut path that starts from your current directory.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+
+# Absolute: cd /pythondecal/course_assignments/homework2
+# Relative: cd ../course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+
+# cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+
+# It would delete your current directory.
+
+# 12) What do the following commands do?
+# git add: moves the file into the staging area
+# git commit: saves your staged changes to the repository
+# git push: uploads your local commits to the remote repository
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+
+# git add . stages everything in the current directory.
+# git add <file> stages only that specific file.
+
+# 14) What do "git status" and "git log -1" do?
+
+# git status shows what's going on right now in your repo.
+# git log -1 shows the most recent commit.
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+
+# Cloning downloads an entire repository for the first time. Pulling upates an existing local
+# repository with new changes from the remoate.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+
+# Putting a folder in a folder. But the new folder was higher. I can't explain it but I spent
+# the entirety of lecture 4 trying to make a "pythondecalfa" folder and put "joannemanuzon" into it.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+
+# I don't have any so far but trust I will have some soon.
+
+# 18) Tell me a fun fact!
+
+# I have a 36 pack of Diet Coke in my closet right now.
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+
+print(10 % 3)
+# Prints 1. Performs division and returns the remainder.
